bdd701e6b5b6747f55372ebec4375cbcf4b095d6
ivan <polyconnect@me.com>
2017-06-16 12:40:22 +0300

    Cleanup

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -32,11 +32,7 @@ FusionEKF::FusionEKF() {
         0, 0.0009, 0,
         0, 0, 0.09;
 
-    /**
-    TODO:
-    * Finish initializing the FusionEKF.
-    * Set the process and measurement noises
-    */
+    
     H_laser_ <<
         1, 0, 0, 0,
         0, 1, 0, 0;
@@ -56,9 +52,7 @@ FusionEKF::FusionEKF() {
         0, 0, 0,    1000;
 }
 
-/**
-* Destructor.
-*/
+// Destructor.
 FusionEKF::~FusionEKF() {}
 
 void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
@@ -81,8 +75,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
             ekf_.x_(0) = init_state(0);
             ekf_.x_(1) = init_state(1);
             
-        }
-        else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+        } else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
             
             //Initialize state.
             ekf_.x_(0) = measurement_pack.raw_measurements_[0];
@@ -91,9 +84,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
         
         previous_timestamp_ = measurement_pack.timestamp_;
       
-    // done initializing, no need to predict or update
-    is_initialized_ = true;
-    return;
+        // done initializing, no need to predict or update
+        is_initialized_ = true;
+        return;
     }
 
     /*****************************************************************************
@@ -138,7 +131,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
     ekf_.Predict();
 
-    //*  Update
+    // Update
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
             
         Eigen::VectorXd cartesianMeas = aux_.PolarToCartesian(measurement_pack.raw_measurements_);
